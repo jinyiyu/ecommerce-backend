cfe576cd850cfb140da17a726907d92422698b39
Jinyi Yu <yujinyiicxk@gmail.com>
2022-07-24 23:45:47 +0100

    get the models and seeds in place

---
+++ src/routes/api/tag-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
+
+// The `/api/tags` endpoint
+
+router.get("/", async (req, res) => {
+  // find all tags - include its associated Product data
+  try {
+    const tags = await Tag.findAll({ include: { model: Product } });
+    if (!tags.length) {
+      res.status(404).json({ success: false, message: "There are no tags..." });
+    }
+    res.json(tags);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const { id } = req.params;
+    const tag = await Tag.findByPk(id, { include: { model: Product } });
+    if (!tag) {
+      res.status(404).json({
+        success: false,
+        message: `Tag id number ${id} does not exist`,
+      });
+    }
+    res.json(tag);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  // create a new tag
+
+  try {
+    const { tag_name } = req.body;
+    const newTag = await Tag.create(tag_name);
+    if (!newTag) {
+      res.status(404).json({
+        success: false,
+        message: `Tag name ${tag_name} does not exist`,
+      });
+      res.json(newTag);
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete("/:id", (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
